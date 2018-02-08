dd4cc6a660a26e660e4b2549f67736dbd81b96f4
Aleksandr Ivanov <aux@elementalconcept.com>
2018-02-08 21:18:24 +0000

    Added GrappaModule boilerplate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
-
+import { GrappaModule } from './grappa/grappa.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    GrappaModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ AppComponent ]
 })
-export class AppModule { }
+export class AppModule {
+}
