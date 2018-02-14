ddf0915bc5e421f17b33b14432267e2758de97c5
Aleksandr Ivanov <aux@elementalconcept.com>
2018-02-14 13:50:02 +0000

    Code clean-up

--- src/app/services/test-client.service.ts
+++ src/app/services/test-client.service.ts
@@ -1,8 +1,6 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 
 import { GET, POST, RestClient } from '../grappa/decorators';
-// import { UID } from '../grappa/core/uid';
 
 @Injectable()
 @RestClient('http://localhost:4200/assets')
@@ -15,10 +13,4 @@ export class TestClientService {
 
   @POST('/users')
   create: (user: any) => any;
-
-  constructor(public http: HttpClient) {
-    /*console.log('TestClientService::constructor!');
-    console.log(this);
-    console.log(UID(this));*/
-  }
 }
