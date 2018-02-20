8ce01ded7cfd91be3d2205bee12155496df3b648
Aleksandr Ivanov <aux@elementalconcept.com>
2018-02-20 11:20:40 +0000

    Updated karma config

--- karma.conf.js
+++ karma.conf.js
@@ -26,8 +26,8 @@ module.exports = function (config) {
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
-    autoWatch: false,
+    autoWatch: true,
     browsers: ['ChromeHeadless'],
-    singleRun: true
+    singleRun: false
   });
 };
