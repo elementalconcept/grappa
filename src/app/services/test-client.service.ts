3de8823448e92d80a3ab96b6cf492d740a9a0a8c
Aleksandr Ivanov <aux@elementalconcept.com>
2018-02-20 11:32:48 +0000

    Updated sample service

--- src/app/services/test-client.service.ts
+++ src/app/services/test-client.service.ts
@@ -1,19 +1,20 @@
 import { Injectable } from '@angular/core';
 
 import { GET, POST, PUT, RestClient } from '../grappa/decorators';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 @RestClient('http://localhost:4200/assets')
 export class TestClientService {
   @GET('/users')
-  list: () => any;
+  list: () => Observable<any>;
 
   @GET('/users/{0}')
-  find: (id: number) => any;
+  find: (id: number) => Observable<any>;
 
   @POST('/users')
-  create: (user: any) => any;
+  create: (user: any) => Observable<any>;
 
   @PUT('/users/{0}')
-  update: (id: number, user: any) => any;
+  update: (id: number, user: any) => Observable<any>;
 }
